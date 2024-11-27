5049085078d8b4a6cee6527c617bc88c05ded718
Crosby Myers <crosby.myers4444@gmail.com>
2024-11-26 19:39:31 -0800

    Added main loop

--- code/main.cpp
+++ code/main.cpp
@@ -2,8 +2,8 @@
 
 int main() {
     // Create a video mode object
-	int pixelWidth = VideoMode::getDesktopMode().width;
-	int pixelHeight = VideoMode::getDesktopMode().height;
+	int pixelWidth = VideoMode::getDesktopMode().width / 2;
+	int pixelHeight = VideoMode::getDesktopMode().height / 2;
 	VideoMode vm(pixelWidth, pixelHeight);
 
 	// Create and open a window for the game
@@ -20,6 +20,45 @@ int main() {
     info.setCharacterSize(14);
     info.setFillColor(Color::White);
 
-    
+    ComplexPlane plane(pixelWidth, pixelHeight);
+
+    while(window.isOpen())
+    {
+        Event event;
+        while (window.pollEvent(event))
+		{
+		    if (event.type == Event::Closed)
+		    {
+					// Quit the game when the window is closed
+					window.close();
+		    }
+            if (event.type == Event::MouseButtonPressed)
+		    {
+			    if (event.mouseButton.button == Mouse::Left)
+			    {
+                    plane.zoomIn();
+                    plane.setCenter(event.mouseButton);
+                }
+                if (event.mouseButton.button == Mouse::Right)
+                {
+                    plane.zoomOut();
+                    plane.setCenter(event.mouseButton);
+                }
+            }
+            if (Keyboard::isKeyPressed(Keyboard::Escape))
+		    {
+			    window.close();
+		    }
+        }
+
+        plane.updateRender();
+        plane.loadText(info);
+
+        window.clear();
+        plane.draw();
+        info.draw();
+        window.display();
+
+    }
     return 0;
 }
