23caa29dbf39f92284771c72ccb854ba7eb339ed
Crosby Myers <crosby.myers4444@gmail.com>
2024-11-26 18:44:47 -0800

    Added arial font

--- code/main.cpp
+++ code/main.cpp
@@ -1,7 +1,19 @@
 #include "ComplexPlane.h"
 
 int main() {
-    cout << "Hello world";
+    // Create a video mode object
+	int pixelWidth = VideoMode::getDesktopMode().width;
+	int pixelHeight = VideoMode::getDesktopMode().height;
+	VideoMode vm(pixelWidth, pixelHeight);
 
+	// Create and open a window for the game
+	RenderWindow window(vm, "Mandelbrot Set", Style::Default);
+
+    Font arial;
+    if (!marioFont.loadFromFile("Fonts/Arial.ttf")) {
+		cout << "Error loading font from file." << endl;
+		return 1;
+	}
+    
     return 0;
 }
